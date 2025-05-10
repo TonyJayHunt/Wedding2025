6cbbe9c8155bd9b89602924ab618ee84cc2c68d8
Tony Hunt <tonyjayhunt@googlemail.com>
2025-05-10 13:22:51 +0100

    updated images and about us

--- js/ImageCarouselScript.js
+++ js/ImageCarouselScript.js
@@ -5,16 +5,27 @@ document.addEventListener('DOMContentLoaded', function() {
   data() {
     return {
     images: [
-      '../../public/images/us01.jpg',
-      '../../public/images/us02.jpg',
-      '../../public/images/us03.jpg',
-      '../../public/images/us04.jpg',
-      '../../public/images/us05.jpg',
-      '../../public/images/us06.jpg',
-      '../../public/images/us07.jpg',
-      '../../public/images/us08.jpg',
-      '../../public/images/us09.jpg',
-      '../../public/images/us10.jpg'
+      '../../public/images/us/us12.jpg',
+      '../../public/images/us/us21.jpg',
+      '../../public/images/us/us01.jpg',
+      '../../public/images/us/us02.jpg',
+      '../../public/images/us/us03.jpg',
+      '../../public/images/us/us04.jpg',
+      '../../public/images/us/us05.jpg',
+      '../../public/images/us/us06.jpg',
+      '../../public/images/us/us07.jpg',
+      '../../public/images/us/us08.jpg',
+      '../../public/images/us/us09.jpg',
+      '../../public/images/us/us10.jpg',
+      '../../public/images/us/us11.jpg',
+      '../../public/images/us/us13.jpg',
+      '../../public/images/us/us14.jpg',
+      '../../public/images/us/us15.jpg',
+      '../../public/images/us/us16.jpg',
+      '../../public/images/us/us17.jpg',
+      '../../public/images/us/us18.jpg',
+      '../../public/images/us/us19.jpg',
+      '../../public/images/us/us20.jpg'      
     ],
     currentImage: 0
     };
